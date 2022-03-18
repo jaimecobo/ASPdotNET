c23e7b21b6d4076fd28990c92d511654b76304ae
Jaime J Cobo <jaimecobo@outlook.com>
2022-03-18 15:21:17 -0400

    PartyRSVP

---
+++ PartyRSVP/PartyRSVP/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyRSVP.Models;
+using System.Diagnostics;
+
+namespace PartyRSVP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Rsvp");
+        }
+        //public string Index()
+        //{
+        //    return "Hello World";
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // Todo: Store guest response, covered in later lessons
+            return View("Thanks", guestResponse);
+        }
+    }
+}
---
+++ PartyRSVP/PartyRSVP/Models/GuestResponse.cs
@@ -0,0 +1,39 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+
+//namespace PartyRSVP.Models
+//{
+//    public class GuestResponse : Controller
+//    {
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+//    }
+//}
+
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyRSVP.Models
+{
+    public class GuestResponse
+    {
+        public int Id { get; set; }
+        [MinLength(3)]
+        [MaxLength(10)]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        [Display(Description = "I can Attend")]
+        public bool? WillAttend { get; set; }
+        public string FullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+    }
+
+}
