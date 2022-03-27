21c6f07197c0860a01f5334ea9f0b27df6d4b530
Jaime J Cobo <jaimecobo@outlook.com>
2022-03-26 21:20:41 -0400

    ContactsEntityFramework

---
+++ ContactsEntityFramework/Models/Contact.cs
@@ -0,0 +1,10 @@
+namespace ContactsEntityFramework.Models{
+    public class Contact{
+        public int Id {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string GetFullName(){
+            return this.FirstName + " " + this.LastName;
+        }
+    }
+}
---
+++ ContactsEntityFramework/Models/ContactsContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsEntityFramework.Models{
+    public class ContactsContext : DbContext {
+        public ContactsContext(DbContextOptions<ContactsContext> options) : base(options) {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
