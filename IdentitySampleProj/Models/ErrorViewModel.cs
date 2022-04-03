2623c1bbe94bdc23eb15b20a6630f4b8f575ff0c
Jaime J Cobo <jaimecobo@outlook.com>
2022-04-02 23:48:58 -0400

    IdentitySampleProj

---
+++ IdentitySampleProj/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IdentitySampleProj.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
