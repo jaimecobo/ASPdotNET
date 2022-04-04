47cb5192c39fa9398e8090be3e8a25e9107a352f
Jaime J Cobo <jaimecobo@outlook.com>
2022-04-03 23:51:03 -0400

    IdentityTesting

---
+++ IdentityTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
