e6b1faee96c61881cfe7fc1a046d187fa189b523
Jaime J Cobo <jaimecobo@outlook.com>
2022-04-08 12:45:45 -0400

    LoggingSample/

---
+++ LoggingSample/Controllers/Test1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoggingSample.Controllers
+{
+    public class Test1 : Controller
+    {
+        //ILogger<Test1> logger;
+        //public Test1( ILogger<Test1>loggerTest1 )
+        //{
+        //    logger = loggerTest1;
+        //}
+        ILogger logger;
+        public Test1(ILoggerFactory loggerTest1)
+        {
+            logger = loggerTest1.CreateLogger("xxx");
+        }
+        public IActionResult Index()
+        {
+            logger.Log(LogLevel.Information, "This is the method I'm calling the index");
+            return View();
+        }
+    }
+}
