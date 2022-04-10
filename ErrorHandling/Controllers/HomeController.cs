37820081f87ec0332ad8486ea35b742f69dcfcbb
Jaime J Cobo <jaimecobo@outlook.com>
2022-04-10 14:08:13 -0400

    ErrorHandling

---
+++ ErrorHandling/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using ErrorHandling.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace ErrorHandling.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public class AboutModel : PageModel
+        {
+            private readonly ILogger _logger;
+
+            public AboutModel(ILogger<AboutModel> logger)
+            {
+                _logger = logger;
+            }
+            public string? Message { get; set; }
+
+            public void OnGet()
+            {
+                Message = $"About page visited at {DateTime.UtcNow.ToLongTimeString()}";
+                _logger.LogInformation(Message);
+            }
+        }
+    }
+}
---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,72 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+
+////Alternative Builder Login Code, instead as what is commented/shown above
+//var builder = WebApplication.CreateBuilder();
+//builder.Host.ConfigureLogging(logging =>
+//{
+//    logging.ClearProviders();
+//    logging.AddConsole();
+//});
+
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+
+app.Run();
+
+
+
+
+
+
+
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//app.Run();
