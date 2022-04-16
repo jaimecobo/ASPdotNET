15186a5b0601bff679d9aefa0680a06bafa372db
Jaime J Cobo <jaimecobo@outlook.com>
2022-04-16 16:55:48 -0400

    CountryApi

---
+++ CountryApi/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace CountryApi.Models
+{
+    public class Country
+    {
+        public long Id { get; set; } 
+        public string? Name { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ CountryApi/Models/CountryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CountryApi.Models
+{
+    public class CountryContext : DbContext
+    {
+        public CountryContext(DbContextOptions<CountryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; } = null!;
+    }
+}
